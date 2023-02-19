637bddae9bf18c2249050dabe9aa472b2741c4ed
alexeyGilyazov <117631127+alexeyGilyazov@users.noreply.github.com>
2023-02-19 18:21:25 +0300

    Add files via upload

---
+++ Examination --Gilyazov/script/custom.js
@@ -0,0 +1,50 @@
+function readMore() {
+    const dots = document.getElementById("dots")
+    const more = document.getElementById("more")
+    const btn = document.getElementById("btn")
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline"
+      btn.innerHTML = "Read More >"
+      more.style.display = "none"
+    }
+    else {
+      dots.style.display = "none"
+      btn.innerHTML = "Hiden <"
+      more.style.display = "inline"
+    }
+  }
+
+  function readMore1() {
+    const dots = document.getElementById("dots1")
+    const more = document.getElementById("more1")
+    const btn = document.getElementById("btn1")
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline"
+      btn.innerHTML = "Read More >"
+      more.style.display = "none"
+    }
+    else {
+      dots.style.display = "none"
+      btn.innerHTML = "Hiden <"
+      more.style.display = "inline"
+    }
+  }
+
+  function readMore2() {
+    const dots = document.getElementById("dots2")
+    const more = document.getElementById("more2")
+    const btn = document.getElementById("btn2")
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline"
+      btn.innerHTML = "Read More >"
+      more.style.display = "none"
+    }
+    else {
+      dots.style.display = "none"
+      btn.innerHTML = "Hiden <"
+      more.style.display = "inline"
+    }
+  }
